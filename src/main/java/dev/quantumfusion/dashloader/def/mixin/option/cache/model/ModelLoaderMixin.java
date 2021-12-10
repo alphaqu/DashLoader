2bb38a2ff2321e2119baf118fc2ef7911953da76
yanchu0612 <yan.gyunashyan@gmail.com>
2021-12-10 14:42:55 +0100

    Revert "Move more code to ModelLoader"
    This reverts commit 8ecf73164facb8c9b6d5be98f4d4f0ea3dfba97e.

--- src/main/java/dev/quantumfusion/dashloader/def/mixin/option/cache/model/ModelLoaderMixin.java
+++ src/main/java/dev/quantumfusion/dashloader/def/mixin/option/cache/model/ModelLoaderMixin.java
@@ -3,8 +3,6 @@ package dev.quantumfusion.dashloader.def.mixin.option.cache.model;
 import com.mojang.datafixers.util.Pair;
 import dev.quantumfusion.dashloader.def.DashDataManager;
 import dev.quantumfusion.dashloader.def.DashLoader;
-import dev.quantumfusion.dashloader.def.api.option.ConfigHandler;
-import dev.quantumfusion.dashloader.def.api.option.Option;
 import dev.quantumfusion.dashloader.def.fallback.MissingDashModel;
 import dev.quantumfusion.dashloader.def.fallback.UnbakedBakedModel;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
@@ -13,14 +11,12 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.color.block.BlockColors;
-import net.minecraft.client.render.model.*;
-import net.minecraft.client.texture.SpriteAtlasTexture;
-import net.minecraft.client.texture.TextureManager;
+import net.minecraft.client.render.model.ModelLoader;
+import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.profiler.Profiler;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -29,14 +25,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.*;
 
 @Mixin(value = ModelLoader.class, priority = 69420)
 public abstract class ModelLoaderMixin {
@@ -55,31 +46,10 @@ public abstract class ModelLoaderMixin {
 	protected abstract void method_4716(BlockState blockState);
 
 	@Mutable
-	@Shadow
-	@Final
-	private Set<Identifier> modelsToLoad;
+	@Shadow @Final private Set<Identifier> modelsToLoad;
 
 	@Mutable
-	@Shadow
-	@Final
-	private Map<Identifier, UnbakedModel> modelsToBake;
-
-	@Mutable
-	@Shadow
-	@Final
-	private Map<Identifier, BakedModel> bakedModels;
-
-	@Shadow
-	@Nullable
-	public abstract BakedModel bake(Identifier id, ModelBakeSettings settings);
-
-	@Shadow
-	@Final
-	private Map<Identifier, Pair<SpriteAtlasTexture, SpriteAtlasTexture.Data>> spriteAtlasData;
-
-	@Shadow
-	@Nullable
-	private SpriteAtlasManager spriteAtlasManager;
+	@Shadow @Final private Map<Identifier, UnbakedModel> modelsToBake;
 
 	@Inject(
 			method = "<init>(Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/client/color/block/BlockColors;Lnet/minecraft/util/profiler/Profiler;I)V",
@@ -132,47 +102,6 @@ public abstract class ModelLoaderMixin {
 			Set<SpriteIdentifier> thing2,
 			Map<Identifier, List<SpriteIdentifier>> map) {
 		if (DashLoader.isRead()) map.clear();
-	}
-
-
-	@Inject(
-			method = "upload",
-			at = @At(value = "HEAD"),
-			cancellable = true
-	)
-	private void upload(TextureManager textureManager, Profiler profiler, CallbackInfoReturnable<SpriteAtlasManager> cir) {
-		final DashDataManager data = DashLoader.getData();
-		if (DashLoader.isRead() && ConfigHandler.optionActive(Option.CACHE_MODEL_LOADER)) {
-			//cache go brr
-			DashLoader.LOGGER.info("Starting apply stage.");
-			//register textures
-			profiler.push("atlas");
-			data.getReadContextData().dashAtlasManager.registerAtlases(textureManager, Option.CACHE_MODEL_LOADER);
-			profiler.swap("baking");
-			profiler.pop();
-
-			final var cacheAtlasManager = data.spriteAtlasManager.getCacheResultData();
-			final var cacheBakedModels = data.bakedModels.getCacheResultData();
-			this.spriteAtlasManager = cacheAtlasManager;
-
-
-			DashLoader.LOGGER.info("Baking fallback models.");
-			AtomicInteger fallback = new AtomicInteger();
-			modelsToBake.forEach((identifier, unbakedModel) -> {
-				if (!(unbakedModel instanceof UnbakedBakedModel)) {
-					cacheBakedModels.put(identifier, bake(identifier, ModelRotation.X0_Y0));
-					if (!identifier.equals(ModelLoader.MISSING_ID)) {
-						fallback.getAndIncrement();
-					}
-				}
-			});
-
-			this.bakedModels = cacheBakedModels;
-			final int size = cacheBakedModels.size();
-			DashLoader.LOGGER.info("Baked {} out of {} models with fallback system. ({}% cache coverage)", fallback.get(), size, (int) ((1 - (fallback.get() / (float) size)) * 100));
-
-			cir.setReturnValue(cacheAtlasManager);
-		}
 	}
 
 }
