aabc65aeb7b36da1bca83dd65b9100211e165e71
alphaqu <yan.gyunashyan@gmail.com>
2022-07-09 18:06:18 +0200

    chore: Do not cache all mipmap levels

--- src/main/java/dev/quantumfusion/dashloader/data/image/DashFabricSprite.java
+++ src/main/java/dev/quantumfusion/dashloader/data/image/DashFabricSprite.java
@@ -7,8 +7,8 @@ import net.fabricmc.fabric.impl.client.texture.FabricSprite;
 
 @DashObject(FabricSprite.class)
 public final class DashFabricSprite extends DashSpriteImpl implements DashSprite {
-	public DashFabricSprite(DashSpriteAnimation animation, int[] images, int x, int y, int width, int height, float uMin, float uMax, float vMin, float vMax) {
-		super(animation, images, x, y, width, height, uMin, uMax, vMin, vMax);
+	public DashFabricSprite(DashSpriteAnimation animation, int image, boolean imageTransparent, int images, int x, int y, int width, int height, float uMin, float uMax, float vMin, float vMax) {
+		super(animation, image, imageTransparent, images, x, y, width, height, uMin, uMax, vMin, vMax);
 	}
 
 	public DashFabricSprite(FabricSprite sprite, RegistryWriter writer) {
