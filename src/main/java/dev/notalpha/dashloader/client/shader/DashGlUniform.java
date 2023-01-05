365e26112504005a3ba70fe88df89e8d35ac89fe
notalpha <yan.gyunashyan@gmail.com>
2023-01-05 14:42:04 +0100

    chore: Remove unused fields.

--- src/main/java/dev/notalpha/dashloader/client/shader/DashGlUniform.java
+++ src/main/java/dev/notalpha/dashloader/client/shader/DashGlUniform.java
@@ -1,36 +1,23 @@
 package dev.notalpha.dashloader.client.shader;
 
-import dev.notalpha.dashloader.io.IOHelper;
-import dev.notalpha.dashloader.misc.UnsafeHelper;
-import dev.notalpha.dashloader.mixin.accessor.GlUniformAccessor;
-import dev.quantumfusion.hyphen.scan.annotations.DataNullable;
 import net.minecraft.client.gl.GlUniform;
 import net.minecraft.client.gl.ShaderProgram;
-
-import java.util.List;
 
 public final class DashGlUniform {
 	public final int count;
 	public final int dataType;
 
-	public final int @DataNullable [] intData;
-	public final float @DataNullable [] floatData;
 	public final String name;
 
-
-	public DashGlUniform(int count, int dataType, int[] intData, float[] floatData, String name) {
+	public DashGlUniform(int count, int dataType, String name) {
 		this.count = count;
 		this.dataType = dataType;
-		this.intData = intData;
-		this.floatData = floatData;
 		this.name = name;
 	}
 
 	public DashGlUniform(GlUniform glUniform) {
 		this.count = glUniform.getCount();
 		this.dataType = glUniform.getDataType();
-		this.intData = glUniform.getIntData() == null ? null : IOHelper.toArray(glUniform.getIntData());
-		this.floatData = glUniform.getFloatData() == null ? null : IOHelper.toArray(glUniform.getFloatData());
 		this.name = glUniform.getName();
 	}
 
