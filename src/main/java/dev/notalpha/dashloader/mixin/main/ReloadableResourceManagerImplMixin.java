f7a5699f34320cc83614606541f1e6ee8c9093bb
notalpha <yan.gyunashyan@gmail.com>
2023-02-28 19:33:06 +0100

    fix: Server resource packs not being told apart

--- src/main/java/dev/notalpha/dashloader/mixin/main/ReloadableResourceManagerImplMixin.java
+++ src/main/java/dev/notalpha/dashloader/mixin/main/ReloadableResourceManagerImplMixin.java
@@ -1,10 +1,10 @@
 package dev.notalpha.dashloader.mixin.main;
 
+import dev.notalpha.dashloader.DashLoader;
 import dev.notalpha.dashloader.client.DashLoaderClient;
 import dev.notalpha.dashloader.misc.ProfilerUtil;
-import net.minecraft.resource.ReloadableResourceManagerImpl;
-import net.minecraft.resource.ResourcePack;
-import net.minecraft.resource.ResourceReload;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.resource.*;
 import net.minecraft.util.Unit;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,6 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
@@ -23,12 +24,17 @@ public class ReloadableResourceManagerImplMixin {
 			at = @At(value = "RETURN", shift = At.Shift.BEFORE))
 	private void reloadDash(Executor prepareExecutor, Executor applyExecutor, CompletableFuture<Unit> initialStage, List<ResourcePack> packs, CallbackInfoReturnable<ResourceReload> cir) {
 		ProfilerUtil.RELOAD_START = System.currentTimeMillis();
-		StringBuilder stringBuilder = new StringBuilder();
-		for (int i = 0; i < packs.size(); i++) {
-			ResourcePack pack = packs.get(i);
-			stringBuilder.append(i).append("$").append(pack.getName());
+		ResourcePackManager manager = MinecraftClient.getInstance().getResourcePackManager();
+		List<String> values = new ArrayList<>();
+		for (String enabledName : manager.getEnabledNames()) {
+			ResourcePackProfile profile = manager.getProfile(enabledName);
+			if (profile != null) {
+				values.add(profile.getName() + "N" + profile.getDisplayName().getString() + "D" + profile.getDescription().getString());
+			}
 		}
-		DashLoaderClient.CACHE.setHash(DigestUtils.md5Hex(stringBuilder.toString()).toUpperCase());
+
+		values.sort(String::compareTo);
+		DashLoaderClient.CACHE.setHash(DigestUtils.md5Hex(values.toString()).toUpperCase());
 		DashLoaderClient.CACHE.start();
 	}
 }
